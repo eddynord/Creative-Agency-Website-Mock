be7b02dc320322808f0497bb1afa43f05b01ef20
Edwin Nord <edwin.nord09@gmail.com>
2022-07-26 21:26:15 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -1,13 +1,23 @@
-const openMenu = document.querySelector('.open_menu');
-const closeMenu = document.querySelector('.close_menu');
+const openMenu = document.querySelector('.open');
+const closeMenu = document.querySelector('.close');
 const menu = document.querySelector('.menu-lightbox');
 const indexPage = document.querySelector('.navigation');
 
-openMenu.addEventListener('click', menuOpen => {
-    if(menu.style.display === 'none' ){
-        indexPage.style.display === 'none'
-    }
-});
+// openMenu.addEventListener('click', menuOpen => {
+//     if(menu.style.display === 'none' ){
+//         indexPage.style.display === 'none'
+//     }
+// });
+
+
+document.querySelector('.open_menu').addEventListener("click", menuOpen);
+
+function menuOpen() {
+    document.querySelector('.menu-lightbox').style.display = 'block';
+    document.querySelector('.navigation').style.display = 'none';
+    document.querySelector('.open_menu').style.display = 'none';
+    // document.querySelector('.close_menu').style.display = 'block';
+}
 
 
 
